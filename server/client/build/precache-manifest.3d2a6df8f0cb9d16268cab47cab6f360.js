dafdbc9fb701da881e2426ae9b006e3855c9bc97
Carlos Gonzalez <cagl18@gmail.com>
2020-08-18 21:44:14 -0400

    building client static files

--- server/client/build/precache-manifest.3d2a6df8f0cb9d16268cab47cab6f360.js
+++ server/client/build/precache-manifest.3d2a6df8f0cb9d16268cab47cab6f360.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "93d711a4897f2d79a046d221dee2b9bf",
+    "revision": "9f8d88bd0e1a7e4d1c66803acc355c12",
     "url": "/index.html"
   },
   {
-    "revision": "52a9ec6c86fedfc534da",
+    "revision": "003ef0945907664a27b9",
     "url": "/static/css/main.0d4ca264.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.140a43f3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "52a9ec6c86fedfc534da",
-    "url": "/static/js/main.c03acd84.chunk.js"
+    "revision": "003ef0945907664a27b9",
+    "url": "/static/js/main.b7d8f908.chunk.js"
   },
   {
     "revision": "79179e0215550d28ac81",
--- server/client/build/service-worker.js
+++ server/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14d74491635d5a3daa14467598cc3c1b.js"
+  "/precache-manifest.3d2a6df8f0cb9d16268cab47cab6f360.js"
 );
 
 self.addEventListener('message', (event) => {
